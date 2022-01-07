c5673c96c027160bace672fe71876e3dc56f9cd4
Dan Fritchman <dan@fritch.mn>
2022-01-07 15:53:40 -0800

    Add Compiled Packages to Revision Control, Largely for Git-Based Dependencies

---
+++ packages/js/vlsir/units.js
@@ -0,0 +1,24 @@
+// source: raw.proto
+/**
+ * @fileoverview
+ * @enhanceable
+ * @suppress {missingRequire} reports error on implicit type usages.
+ * @suppress {messageConventions} JS Compiler reports an error if a variable or
+ *     field starts with 'MSG_' and isn't a translatable message.
+ * @public
+ */
+// GENERATED CODE -- DO NOT EDIT!
+/* eslint-disable */
+// @ts-nocheck
+
+goog.provide('proto.vlsir.raw.Units');
+
+/**
+ * @enum {number}
+ */
+proto.vlsir.raw.Units = {
+  MICRO: 0,
+  NANO: 1,
+  ANGSTROM: 2
+};
+
