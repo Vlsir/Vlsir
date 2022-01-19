e7b04764609bc33474b90389722a3b662d27b4e2
Sean Huang <sehuang@berkeley.edu>
2022-01-19 14:55:09 -0800

    Populated dc and ac methods

--- VlsirTools/vlsirtools/xyce.py
+++ VlsirTools/vlsirtools/xyce.py
@@ -120,11 +120,99 @@ class Sim:
 
     def ac(self, an: vlsir.spice.AcInput) -> vlsir.spice.AcResult:
         """ Run an AC analysis. """
-        raise NotImplementedError
+        
+        # Unpack the `AcInput`
+        analysis_name = an.analysis_name or "ac"
+        if len(an.ctrl):
+            raise NotImplementedError  # FIXME!
+
+        # Copy and append to the existing DUT netlist
+        shutil.copy("dut", f"{analysis_name}.sp")
+        netlist = open(f"{analysis_name}.sp", "a")
+
+        # Write the analysis command
+        npts = an.npts
+        fstart = an.fstart
+        fstop = an.fstop
+        netlist.write(f".ac DEC {npts} {fstart} {fstop} \n\n")
+
+        # FIXME: always saving everything, no matter what
+        # Note `csv` output-formatting is encoded here
+        netlist.write(".print ac format=csv v(*) i(*) \n\n")
+
+        # And don't forget - the thing SPICE can't live without - END!
+        netlist.write(".end \n\n")
+        netlist.flush()
+
+        # Do the real work, running the simulation
+        self.run_xyce_process(analysis_name)
+
+        # Read the results from CSV
+        with open(f"{analysis_name}.sp.FD.csv", "r") as csv_handle:
+            (signals, data) = self.read_csv(csv_handle)
+            
+        # Separate Frequency vector
+        n_sigs = len(signals)       # Get length of signals vector because...
+        freq = data[::n_sigs]       # ...every n-th data pt is a freq pt
+        del data[::n_sigs]          # Clean the list of data of all frequencies
+        signals.pop(0)              # Remove "Frequency" from list of signals
+        
+        # Build ComplexNumbers for each data point
+        reals = data[::2]
+        imags = data[1::2]
+        if not len(reals) == len(imags): # Sanity check
+            raise RuntimeError("Unpaired complex number in data")
+        cplx_data = [vlsir.spice.ComplexNum(re=real, im=imag) for real, imag
+                     in zip(reals, imags)]
+
+        # And arrange them in an `AcResult`
+        return vlsir.spice.AcResult(freq=freq, signals=signals, data=cplx_data)
 
     def dc(self, an: vlsir.spice.DcInput) -> vlsir.spice.DcResult:
         """ Run a DC analysis. """
-        raise NotImplementedError
+        
+        # Unpack the `DcInput`
+        analysis_name = an.analysis_name or "dc"
+        sweep = an.sweep        
+        
+        if len(an.ctrl):
+            raise NotImplementedError  # FIXME!
+
+        # Copy and append to the existing DUT netlist
+        shutil.copy("dut", f"{analysis_name}.sp")
+        netlist = open(f"{analysis_name}.sp", "a")
+
+        # Write the analysis command
+        param = an.indep_name
+        ## Interpret the sweep 
+        sweep_type = an.WhichOneOf("sweep")
+        if sweep_type == "linear":
+            netlist.write(f".dc LIN {param} {sweep.start} {sweep.stop} {sweep.step}\n\n")
+        elif sweep_type == "log":
+            netlist.write(f".dc DEC {param} {sweep.start} {sweep.stop} {sweep.npts}\n\n")
+        elif sweep_type == "points":
+            netlist.write(f".dc {param} LIST {' '.join([str(pt) for pt in sweep.points])}\n\n")
+        else:
+            raise ValueError("Invalid sweep type")
+        
+
+        # FIXME: always saving everything, no matter what
+        # Note `csv` output-formatting is encoded here
+        netlist.write(".print dc format=csv v(*) i(*) \n\n")
+
+        # And don't forget - the thing SPICE can't live without - END!
+        netlist.write(".end \n\n")
+        netlist.flush()
+
+        # Do the real work, running the simulation
+        self.run_xyce_process(analysis_name)
+
+        # Read the results from CSV
+        with open(f"{analysis_name}.sp.csv", "r") as csv_handle:
+            (signals, data) = self.read_csv(csv_handle)
+
+        # And arrange them in an `OpResult`
+        return vlsir.spice.DcResult(signals=signals, data=data)
 
     def op(self, an: vlsir.spice.OpInput) -> vlsir.spice.OpResult:
         """ Run an operating-point analysis. 
