31380bed358cfd26adc43592df7599779e03a5ad
Dan Fritchman <dan@fritch.mn>
2022-02-03 10:34:32 -0800

    Add Initial Rust Bindings

---
+++ bindings/rust/build.rs
@@ -0,0 +1,32 @@
+//!
+//! # Build Module
+//!
+//! Primarily expands protobuf definitions, adding a number of annotations.
+//!
+
+use prost_build;
+
+fn main() {
+    // Create the Prost config
+    let mut config = prost_build::Config::new();
+
+    // Add serde traits
+    config.type_attribute(
+        ".",
+        "#[derive(serde_derive::Serialize, serde_derive::Deserialize)]",
+    );
+
+    // Add our custom annotations
+    // config.type_attribute("example", "#[serde(tag = \"type\")]");
+    // config.field_attribute("example", "#[serde(flatten)]");
+
+    // And build!
+    let src = [
+        "../../protos/utils.proto",
+        "../../protos/raw.proto",
+        "../../protos/circuit.proto",
+        "../../protos/tetris.proto",
+        "../../protos/spice.proto",
+    ];
+    config.compile_protos(&src, &["../../protos/"]).unwrap();
+}
---
+++ bindings/rust/readme.md
@@ -0,0 +1,4 @@
+
+# Vlsir 
+
+Rust bindings to the [Vlsir](https://github.com/dan-fritchman/Vlsir) data-schemas for chip design. 
