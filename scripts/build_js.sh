6859f7209d9df726cde682ecaa5af40264162cdb
ThomasPluck <tompluck@mail.com>
2023-04-11 11:31:59 +0100

    fixed js script

--- scripts/build_js.sh
+++ scripts/build_js.sh
@@ -5,6 +5,9 @@
 # Must be run from the root of the Vlsir directory.
 # 
 
+# Make necessary directory
+mkdir -p bindings/js/vlsir
+
 # Protobuf Compilation
 protoc -I=./protos \
     --js_out=./bindings/js/vlsir \
