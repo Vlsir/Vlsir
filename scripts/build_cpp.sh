8a68c2292e893bc3a5826ee3855d4dc73b6e4060
ThomasPluck <tompluck@mail.com>
2023-04-11 11:29:11 +0100

    fixed build_cpp script

--- scripts/build_cpp.sh
+++ scripts/build_cpp.sh
@@ -5,6 +5,9 @@
 # Must be run from the root of the Vlsir directory.
 # 
 
+# Make necessary directory
+mkdir -p bindings/cpp/vlsir
+
 # Protobuf Compilation
 protoc -I=./protos \
     --cpp_out=./bindings/cpp/vlsir \
