d812457f8b5b820b0a3451c8a6deef833ee88665
Dan Fritchman <dan@fritch.mn>
2023-06-26 16:48:59 -0700

    Schema & Bindings changes for Module.path. Clean up Layout binding generation.

--- scripts/build_python.sh
+++ scripts/build_python.sh
@@ -11,10 +11,11 @@ set -eo
 # Protobuf Compilation
 protoc -I=./protos \
     --python_out=./bindings/python/vlsir \
-    ./protos/*.proto
+    ./protos/*.proto \
+    ./protos/**/*.proto
 
 # Sadly `protoc` doesn't seem to know how Python3 imports work. Correct them. 
-2to3 -wn -f import bindings/python/vlsir/*.py
+2to3 -wn -f import bindings/python/vlsir/*.py bindings/python/vlsir/**/*.py
 
 # Run language-specific formatting
 black bindings/python
