585f6f3b625a60e726e2c77dce953957fae1140f
Arya Reais-Parsi <aryap@berkeley.edu>
2023-08-23 17:06:31 -0700

    black format

--- util/proto2spice.py
+++ util/proto2spice.py
@@ -18,16 +18,36 @@ import vlsirtools as tools
 
 
 def define_options(optparser: optparse.OptionParser):
-    optparser.add_option('-l', '--library', dest='library', default=None,
-                         help='path to vlsir.raw.Library, in either text- or'
-                              'binary-format (will try binary first)')
-    optparser.add_option('-p', '--package', dest='package', default=None,
-                         help='path to vlsir.circuit.Package, in either text-'
-                              ' or binary-format proto.')
-    optparser.add_option('-o', '--output', dest='output', default='output.sp',
-                         help='destination path of spice netlist')
-    optparser.add_option('-f', '--format', dest='format', default='xyce',
-                         help='spice format/style/dialect to write')
+    optparser.add_option(
+        "-l",
+        "--library",
+        dest="library",
+        default=None,
+        help="path to vlsir.raw.Library, in either text- or"
+        "binary-format (will try binary first)",
+    )
+    optparser.add_option(
+        "-p",
+        "--package",
+        dest="package",
+        default=None,
+        help="path to vlsir.circuit.Package, in either text-"
+        " or binary-format proto.",
+    )
+    optparser.add_option(
+        "-o",
+        "--output",
+        dest="output",
+        default="output.sp",
+        help="destination path of spice netlist",
+    )
+    optparser.add_option(
+        "-f",
+        "--format",
+        dest="format",
+        default="xyce",
+        help="spice format/style/dialect to write",
+    )
 
 
 # Extracts the circuit definitions in a layout Library into a circuit Package,
@@ -35,17 +55,17 @@ def define_options(optparser: optparse.OptionParser):
 def load_library_into_package(path: str) -> vlsir_circuit.Package:
     library_pb = None
     try:
-        with open(path, 'rb') as f:
+        with open(path, "rb") as f:
             library_pb = vlsir_layout.Library()
             library_pb.ParseFromString(f.read())
     except FileNotFoundError as err:
         raise err
     except:
         try:
-            with open(path, 'r') as f:
+            with open(path, "r") as f:
                 library_pb = text_format.Parse(f.read(), vlsir_layout.Library())
         except Exception as err:
-            raise IOError(f'Could not load input Library at {path}: {err}')
+            raise IOError(f"Could not load input Library at {path}: {err}")
 
     package_pb = vlsir_circuit.Package()
     package_pb.modules.extend([cell.module for cell in library_pb.cells])
@@ -55,18 +75,17 @@ def load_library_into_package(path: str) -> vlsir_circuit.Package:
 def load_package(path: str) -> vlsir_circuit.Package:
     package_pb = None
     try:
-        with open(path, 'rb') as f:
+        with open(path, "rb") as f:
             package_pb = vlsir_circuit.Package()
             package_pb.ParseFromString(f.read())
     except FileNotFoundError as err:
         raise err
     except:
         try:
-            with open(path, 'r') as f:
-                package_pb = text_format.Parse(
-                    f.read(), vlsir_circuit.Package())
+            with open(path, "r") as f:
+                package_pb = text_format.Parse(f.read(), vlsir_circuit.Package())
         except Exception as err:
-            raise IOError(f'Could not load input Package at {path}: {err}')
+            raise IOError(f"Could not load input Package at {path}: {err}")
 
     return package_pb
 
@@ -83,9 +102,8 @@ def process(options: optparse.Values):
     # This is not yet implemented.
     netlist_options = None
 
-    with open(options.output, 'w') as output:
-        tools.netlist(
-            package_pb, output, fmt=options.format, opts=netlist_options)
+    with open(options.output, "w") as output:
+        tools.netlist(package_pb, output, fmt=options.format, opts=netlist_options)
 
 
 def main():
@@ -95,5 +113,5 @@ def main():
     process(options)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
